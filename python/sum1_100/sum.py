ec710600e94fef2c0ad4ca74f53a51a067b00d50
Fudong He <fudong.he@cern.ch>
2019-08-04 15:45:03 +0200

    study

---
+++ python/sum1_100/sum.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-  # 加入中文模块UTF-8
+#!/usr/bin/env python3   # 告诉电脑这个文件用python3打开 
+
+sum = 0
+counter = 1
+while counter < 101:
+    sum = sum + counter
+    counter = counter + 1
+ 
+print("从1加到100 等于", sum)
+
